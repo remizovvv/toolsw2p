462c62384f8b631b6d3c98c306777212443183c5
Yuriy Arkhipov <omadonex@yandex.ru>
2018-02-08 16:40:20 +0300

    wip

---
+++ src/Interfaces/IRepository.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: omadonex
+ * Date: 06.02.2018
+ * Time: 21:34
+ */
+
+namespace Omadonex\ToolsW2p\Interfaces;
+
+interface IRepository
+{
+    public function find($id, $relations = []);
+
+    public function all($relations = []);
+}
