82f52b38181b0b0537b7a0b1d491d84cbcdc7061
Yuriy Arkhipov <omadonex@yandex.ru>
2018-02-15 10:18:00 +0300

    wip

---
+++ src/Classes/Utils/LicenseUtils.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: omadonex
+ * Date: 12.02.2018
+ * Time: 15:42
+ */
+
+namespace Omadonex\ToolsW2p\Classes\Utils;
+
+class LicenseUtils
+{
+    const PERMISSION_CRM = 1;
+    const PERMISSION_PRODUCTS = 2;
+    const PERMISSION_EXCHANGE = 3;
+}
