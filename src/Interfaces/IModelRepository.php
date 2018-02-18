bdbe2eebfed2957f7b8be9aab81b7cf24da5c4c2
Yuriy Arkhipov <omadonex@yandex.ru>
2018-02-18 23:18:36 +0300

    wip

--- src/Interfaces/IModelRepository.php
+++ src/Interfaces/IModelRepository.php
@@ -59,5 +59,5 @@ interface IModelRepository
 
     public function listResource($relations = true, $trashed = null, $active = null, $paginate = true);
 
-    public function agrCount();
+    public function agrCount($trashed = null, $active = null);
 }
