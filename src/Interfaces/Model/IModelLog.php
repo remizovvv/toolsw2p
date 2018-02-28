2efdc609919c7dc99a926db9c2f9ac6b8e4815d1
Yuriy Arkhipov <omadonex@yandex.ru>
2018-02-28 15:18:05 +0300

    wip

---
+++ src/Interfaces/Model/IModelLog.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: omadonex
+ * Date: 06.02.2018
+ * Time: 21:34
+ */
+
+namespace Omadonex\ToolsW2p\Interfaces\Model;
+
+interface IModelLog
+{
+    public function saveToLog(IModelService $service);
+}
